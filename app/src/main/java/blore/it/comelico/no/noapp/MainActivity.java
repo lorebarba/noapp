f0c69f3d23b6011b69cf2ac027f154f4fee7a7c3
lore-cubu <me@blore.it>
2016-03-16 09:53:26 +0100

    prima release

--- app/src/main/java/blore/it/comelico/no/noapp/MainActivity.java
+++ app/src/main/java/blore/it/comelico/no/noapp/MainActivity.java
@@ -1,13 +1,23 @@
 package blore.it.comelico.no.noapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPlayer  = MediaPlayer.create(this, R.raw.no);
+
+    }
+
+    public void noClick(View view) {
+    mPlayer.start();
     }
 }
